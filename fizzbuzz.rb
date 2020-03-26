e6e258f9cbda418763a759d60ef1a4a75a8f4965
James Jensen <jnjensen@live.com>
2020-03-26 01:47:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,3 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(num)
-  if num.to_i % 3
-    puts "Fizz"
-  elsif num.to_i % 5
-    puts "Buzz"
-end
